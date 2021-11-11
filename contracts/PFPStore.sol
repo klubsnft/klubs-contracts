de33e89f47d7f44604d04ba15fa0131bbd6ce8f1
TheGreatHB <kavanaboy@gmail.com>
2021-11-11 17:47:11 +0900

    Apply prettier

--- contracts/PFPStore.sol
+++ contracts/PFPStore.sol
@@ -22,16 +22,16 @@ contract PFPStore is Ownable, IPFPStore {
         mix = _mix;
     }
 
-    function setFee(uint256 _fee) onlyOwner external {
-        require(fee < 9 * 1e3);     //max 90%
+    function setFee(uint256 _fee) external onlyOwner {
+        require(fee < 9 * 1e3); //max 90%
         fee = _fee;
     }
 
-    function setFeeReceiver(address _receiver) onlyOwner external {
+    function setFeeReceiver(address _receiver) external onlyOwner {
         feeReceiver = _receiver;
     }
 
-    function setPFPs(address _pfps) onlyOwner external {
+    function setPFPs(address _pfps) external onlyOwner {
         pfps = _pfps;
     }
 
@@ -40,13 +40,18 @@ contract PFPStore is Ownable, IPFPStore {
         _;
     }
 
-    function distributeReward(address addr, uint256 id, address to, uint256 amount) private {
+    function distributeReward(
+        address addr,
+        uint256 id,
+        address to,
+        uint256 amount
+    ) private {
         uint256 _fee = amount.mul(fee).div(1e4);
-        if(_fee > 0) mix.transfer(feeReceiver, _fee);
+        if (_fee > 0) mix.transfer(feeReceiver, _fee);
 
         (address receiver, uint256 royalty) = pfps.royalties(addr);
         uint256 _royalty = amount.mul(royalty).div(1e4);
-        if(_royalty > 0) mix.transfer(receiver, _royalty);
+        if (_royalty > 0) mix.transfer(receiver, _royalty);
 
         mix.transfer(to, amount.sub(_fee).sub(_royalty));
 
@@ -61,27 +66,27 @@ contract PFPStore is Ownable, IPFPStore {
     }
     mapping(address => mapping(uint256 => Sale)) public sales;
 
-    function sell(address addr, uint256 id, uint256 price) whitelist(addr) external {
+    function sell(
+        address addr,
+        uint256 id,
+        uint256 price
+    ) external whitelist(addr) {
         require(price > 0);
-        
+
         IKIP17 nft = IKIP17(addr);
         require(nft.ownerOf(id) == msg.sender);
         nft.transferFrom(msg.sender, address(this), id);
 
-        sales[addr][id] = Sale({
-            seller: msg.sender,
-            price: price
-        });
+        sales[addr][id] = Sale({seller: msg.sender, price: price});
 
         emit Sell(addr, id, msg.sender, price);
     }
 
-    function checkSelling(address addr, uint256 id) view external returns (bool) {
+    function checkSelling(address addr, uint256 id) external view returns (bool) {
         return sales[addr][id].seller != address(0);
     }
 
     function buy(address addr, uint256 id) external {
-
         Sale memory sale = sales[addr][id];
         require(sale.seller != address(0));
 
@@ -94,7 +99,6 @@ contract PFPStore is Ownable, IPFPStore {
     }
 
     function cancelSale(address addr, uint256 id) external {
-
         address seller = sales[addr][id].seller;
         require(seller == msg.sender);
 
@@ -111,28 +115,32 @@ contract PFPStore is Ownable, IPFPStore {
     }
     mapping(address => mapping(uint256 => OfferInfo[])) public offers;
 
-    function offerCount(address addr, uint256 id) view external returns (uint256) {
+    function offerCount(address addr, uint256 id) external view returns (uint256) {
         return offers[addr][id].length;
     }
 
-    function offer(address addr, uint256 id, uint256 price) whitelist(addr) external returns (uint256 offerId) {
+    function offer(
+        address addr,
+        uint256 id,
+        uint256 price
+    ) external whitelist(addr) returns (uint256 offerId) {
         require(price > 0);
 
         OfferInfo[] storage os = offers[addr][id];
         offerId = os.length;
 
-        os.push(OfferInfo({
-            offeror: msg.sender,
-            price: price
-        }));
+        os.push(OfferInfo({offeror: msg.sender, price: price}));
 
         mix.transferFrom(msg.sender, address(this), price);
 
         emit Offer(addr, id, offerId, msg.sender, price);
     }
 
-    function cancelOffer(address addr, uint256 id, uint256 offerId) external {
-
+    function cancelOffer(
+        address addr,
+        uint256 id,
+        uint256 offerId
+    ) external {
         OfferInfo[] storage os = offers[addr][id];
         OfferInfo memory _offer = os[offerId];
         require(_offer.offeror == msg.sender);
@@ -143,8 +151,11 @@ contract PFPStore is Ownable, IPFPStore {
         emit CancelOffer(addr, id, offerId, msg.sender);
     }
 
-    function acceptOffer(address addr, uint256 id, uint256 offerId) external {
-
+    function acceptOffer(
+        address addr,
+        uint256 id,
+        uint256 offerId
+    ) external {
         OfferInfo[] storage os = offers[addr][id];
         OfferInfo memory _offer = os[offerId];
 
@@ -164,21 +175,21 @@ contract PFPStore is Ownable, IPFPStore {
     }
     mapping(address => mapping(uint256 => AuctionInfo)) public auctions;
 
-    function checkAuction(address addr, uint256 id) view external returns (bool) {
+    function checkAuction(address addr, uint256 id) external view returns (bool) {
         return auctions[addr][id].seller != address(0);
     }
 
-    function auction(address addr, uint256 id, uint256 startPrice, uint256 endBlock) whitelist(addr) external {
-
+    function auction(
+        address addr,
+        uint256 id,
+        uint256 startPrice,
+        uint256 endBlock
+    ) external whitelist(addr) {
         IKIP17 nft = IKIP17(addr);
         require(nft.ownerOf(id) == msg.sender);
         nft.transferFrom(msg.sender, address(this), id);
 
-        auctions[addr][id] = AuctionInfo({
-            seller: msg.sender,
-            startPrice: startPrice,
-            endBlock: endBlock
-        });
+        auctions[addr][id] = AuctionInfo({seller: msg.sender, startPrice: startPrice, endBlock: endBlock});
 
         emit Auction(addr, id, msg.sender, startPrice, endBlock);
     }
@@ -195,19 +206,22 @@ contract PFPStore is Ownable, IPFPStore {
 
         emit CancelAuction(addr, id, msg.sender);
     }
-    
+
     struct Bidding {
         address bidder;
         uint256 price;
     }
     mapping(address => mapping(uint256 => Bidding[])) public biddings;
 
-    function biddingCount(address addr, uint256 id) view external returns (uint256) {
+    function biddingCount(address addr, uint256 id) external view returns (uint256) {
         return biddings[addr][id].length;
     }
 
-    function bid(address addr, uint256 id, uint256 price) external returns (uint256 biddingId) {
-
+    function bid(
+        address addr,
+        uint256 id,
+        uint256 price
+    ) external returns (uint256 biddingId) {
         AuctionInfo memory _auction = auctions[addr][id];
         require(_auction.seller != address(0) && block.number < _auction.endBlock);
 
@@ -222,10 +236,7 @@ contract PFPStore is Ownable, IPFPStore {
             mix.transfer(bestBidding.bidder, bestBidding.price);
         }
 
-        bs.push(Bidding({
-            bidder: msg.sender,
-            price: price
-        }));
+        bs.push(Bidding({bidder: msg.sender, price: price}));
 
         mix.transferFrom(msg.sender, address(this), price);
 
@@ -233,7 +244,6 @@ contract PFPStore is Ownable, IPFPStore {
     }
 
     function claim(address addr, uint256 id) external {
-
         AuctionInfo memory _auction = auctions[addr][id];
         Bidding[] memory bs = biddings[addr][id];
         Bidding memory bidding = bs[bs.length.sub(1)];
