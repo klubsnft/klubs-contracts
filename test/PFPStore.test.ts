5f8fdac63e1f0789b9bd0016b0b94e27a0b4323f
TheGreatHB <kavanaboy@gmail.com>
2021-11-13 01:26:17 +0900

    Update PFPStore.test.ts

--- test/PFPStore.test.ts
+++ test/PFPStore.test.ts
@@ -10,7 +10,7 @@ const { MaxUint256, Zero, AddressZero } = constants;
 
 const setupTest = async () => {
     const signers = await ethers.getSigners();
-    const [deployer, alice, bob, carol, dan] = signers;
+    const [deployer, alice, bob, carol, dan, pfpManager, pfp2Manager, pfp3Manager] = signers;
 
     const TestMix = await ethers.getContractFactory("TestMix");
     const mix = (await TestMix.deploy()) as TestMix;
@@ -23,8 +23,24 @@ const setupTest = async () => {
     const PFPs = await ethers.getContractFactory("PFPs");
     const pfps = (await PFPs.deploy()) as PFPs;
 
+    await pfps.addByOwner(pfp.address, pfpManager.address);
+    await pfps.addByOwner(pfp2.address, pfp2Manager.address);
+
     const PFPStore = await ethers.getContractFactory("PFPStore");
     const pfpStore = (await PFPStore.deploy(pfps.address, mix.address)) as PFPStore;
+
+    await mix.mint(alice.address, 100000);
+    await mix.mint(bob.address, 100000);
+    await mix.mint(carol.address, 100000);
+    await mix.mint(dan.address, 100000);
+
+    await mix.approve(pfpStore.address, MaxUint256);
+    await mix.connect(alice).approve(pfpStore.address, MaxUint256);
+    await mix.connect(bob).approve(pfpStore.address, MaxUint256);
+    await mix.connect(carol).approve(pfpStore.address, MaxUint256);
+    await mix.connect(dan).approve(pfpStore.address, MaxUint256);
+
+    await mineTo((await pfpStore.auctionExtensionInterval()).toNumber());
 
     return {
         deployer,
@@ -38,6 +54,9 @@ const setupTest = async () => {
         pfp3,
         pfps,
         pfpStore,
+        pfpManager,
+        pfp2Manager,
+        pfp3Manager,
     };
 };
 
@@ -46,7 +65,56 @@ describe("PFPStore", () => {
         await ethers.provider.send("hardhat_reset", []);
     });
 
-    it("-", async () => {
-        const { deployer, alice, bob, carol, dan, mix, pfp, pfp2, pfp3, pfps, pfpStore } = await setupTest();
+    it("should be that basic functions and variables related with fee work properly", async () => {
+        const { deployer, alice, bob, carol, dan, mix, pfp, pfps, pfpManager, pfpStore } = await setupTest();
+
+        await expect(pfpStore.connect(alice).setFee(100)).to.be.reverted;
+        await expect(pfpStore.setFee(9000)).to.be.reverted;
+
+        expect(await pfpStore.fee()).to.be.equal(25);
+        await pfpStore.setFee(8999);
+        expect(await pfpStore.fee()).to.be.equal(8999);
+
+        await expect(pfpStore.connect(alice).setFeeReceiver(alice.address)).to.be.reverted;
+
+        expect(await pfpStore.feeReceiver()).to.be.equal(deployer.address);
+        await pfpStore.setFeeReceiver(alice.address);
+        expect(await pfpStore.feeReceiver()).to.be.equal(alice.address);
+
+        await pfp.massMint(bob.address, 4);
+        await pfp.connect(bob).setApprovalForAll(pfpStore.address, true);
+
+        await pfpStore.connect(bob).sell([pfp.address], [0], [1000]);
+        await expect(() => pfpStore.connect(carol).buy([pfp.address], [0])).to.changeTokenBalances(
+            mix,
+            [alice, bob, pfpManager, carol],
+            [899, 101, 0, -1000]
+        );
+        expect(await pfp.ownerOf(0)).to.be.equal(carol.address);
+
+        await pfpStore.connect(bob).createAuction(pfp.address, 1, 10000, (await getBlock()) + 310);
+        await pfpStore.setFee(25);
+        await pfps.connect(pfpManager).setRoyalty(pfp.address, pfpManager.address, 900);
+        await expect(() => pfpStore.connect(dan).bid(pfp.address, 1, 10000)).to.changeTokenBalances(
+            mix,
+            [dan, pfpStore],
+            [-10000, 10000]
+            );
+        await mine(310);
+        await expect(() => pfpStore.claim(pfp.address, 1)).to.changeTokenBalances(
+            mix,
+            [alice, bob, pfpManager, pfpStore],
+            [25, 9075 ,900, -10000]
+        );
+
+        await expect(() => pfpStore.connect(alice).makeOffer(pfp.address, 2, 3000)).to.changeTokenBalance(mix, alice, -3000);
+        await pfpStore.setFeeReceiver(deployer.address);
+        await pfps.connect(pfpManager).setRoyalty(pfp.address, dan.address, 110)
+
+        await expect(() => pfpStore.connect(bob).acceptOffer(pfp.address, 2, 0)).to.changeTokenBalances(
+            mix,
+            [deployer, alice, bob, pfpManager, dan, pfpStore],
+            [7, 0, 2960, 0, 33, -3000]
+        );
     });
 });
